fcc88241090c73d6ab7256fb74634805871a07f2
Nuutti Rinne <zeroviewzero@gmail.com>
2025-11-18 13:09:18 +0200

    Updated config to support GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), wasm(), topLevelAwait()],
+  base: "/DMG-2025/"
 })
